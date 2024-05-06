86efd92f2612facc1c60e9e337b0dd03aa117f89
Aisyahtul1 <164297508+Aisyahtul1@users.noreply.github.com>
2024-05-06 07:09:45 -0800

    pindah file

---
+++ project1/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/tes-git', function () {
+//     return "Hallo, ini cuma coba pakai git";
+// });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
